c606d24ddc03a804511ab95e0c3ac8928592e0d2
viganzeqiri <viganzeqirii@gmail.com>
2023-06-11 12:49:03 +0200

    Next js basic template setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
+}
